7d3f78c36e46886475fbbbca205ee5e72530c892
Edgar Muyodi <edgarmuyodi@gmail.com>
2024-09-04 11:08:31 +0000

    secret key

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", " postgres://uqp7a68p1k1:v3TSTbf7O1Er@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/deck_mardi_twins_586610")
-
-os.environ.setdefault("SECRET_KEY", "<your_choice_of_secret_key>")
+    "DATABASE_URL", "postgres://uqp7a68p1k1:v3TSTbf7O1Er@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/deck_mardi_twins_586610")
+os.environ.setdefault(
+    "SECRET_KEY", "Yulia123")
